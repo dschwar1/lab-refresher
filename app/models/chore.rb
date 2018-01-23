80a215163cb85765c78f081297fef6b400e2e95a
Dylan Schwartz <dschwar1@andrew.cmu.edu>
2018-01-23 22:32:50 +0000

    finished up all models, all tests started passing so I guess no problem with the environment after all, just need to wait for profh's server, until then this is on my personal github I guess

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,2 +1,21 @@
 class Chore < ActiveRecord::Base
+    belongs_to :child
+    belongs_to :task
+    
+    validates_date :due_on
+    
+    scope :by_task, -> { Chore.joins(:task).order('name') }
+    scope :chronological, -> { order('due_on') }
+    scope :pending, -> { where(completed: false) } 
+    scope :done, -> { where(completed: true) }
+    scope :upcoming, -> {where('due_on >= ?', Date.today) }
+    scope :past, -> { where('due_on < ?', Date.today) }
+    
+    def status
+        unless self.completed
+            return 'Pending'
+        end
+        return 'Completed'
+    end
+    
 end
